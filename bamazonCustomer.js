77284b7593ae1967bc0ce4652d7b68e05192ebf8
Nelson Diaz <abnerdc86@gmail.com>
2018-04-17 14:50:40 -0400

    make the displayInventory() function works

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -46,82 +46,19 @@ function start() {
     // buyItem(answer.itemId, answer.quantity);
   });
 }
-/* function postAuction() {
-  inquirer.prompt(
-    [{
-      name: "item",
-      type: "input",
-      message: "What is the item you wish to submit?"
-    }, {
-      name: "category",
-      type: "input",
-      message: "What category do you like to place it in?"
-    }, {
-      name: "startingBid",
-      type: "input",
-      message: "What would you like the startig bid to be?",
-      validate: function (value) {
-        if (isNaN(value) === false) {
-          return true;
-        } else {
-          return false;
-        }
-      }
-    }
-    ]
-  ).then(function (answer) {
-    connection.query("INSERT INTO auctions SET ?", {
-      itemname: answer.item,
-      category: answer.category,
-      startingbid: answer.startingBid,
-      highestbid: answer.startingBid
-    },
-      function (err, res) {
-        if (err) throw err;
-        console.log("Your auction was created successfuly!");
-        start();
-      });
-  });
-}
-
-function bidAuction() {
-  connection.query("SELECT * FROM auctions", function (err, res) {
-    // res.forEach(element => {
-    //   console.log(element.itemname);
-    // });
-    inquirer.prompt({
-      name: "choice",
-      type: "list",
-      message: "What auction would you like to place a bid on?",
-      choices: function (value) {
-        var choiceArray = [];
-        res.forEach(element => {
-          choiceArray.push(element.itemname);
-        });
-        return choiceArray;
-      },
-    }).then(function (answer) {
-      console.log("this is the item you chose: " + answer.choice);
-    });
-  });
-  start();
-}
- */
 
 function displayInventory() {
   let data = [];
-  let output;
   connection.query(
     "SELECT item_id, product_name, price FROM products",
     function (err, res) {
       if (err) throw err;
+      data.push(["ID", "Product", "Price"]);
       res.forEach(element => {
         let row = [ element.item_id, element.product_name, element.price];
         data.push(row);
       });
-      output = table(data);
-      console.log(output);
-      console.log(table(res));
+      console.log(table(data));
       start();
     });
 }
