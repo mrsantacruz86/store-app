8126ae6b47df4811b414566f15028814d992fb8f
Nelson Diaz <abnerdc86@gmail.com>
2018-04-18 00:34:28 -0400

    finishing the customet app

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -4,58 +4,55 @@ const { table } = require('table');
 const conn = require('./connection');
 console.log('Loading Connection...');
 
-var idList = [];
 
 var connection = mysql.createConnection(conn.config);
-connection.connect(function (err)
-{
-  console.log(err);
+connection.connect(function (err) {
+  if(err) throw err;
   console.log(`Connected as id: ${connection.threadId}`);
   //Display a table with all the items
   start();
 });
 
-function start()
-{
+function start() {
+  var test = function () {
+    //Promt the user to select an item by its ID
+    console.log("Choose Item!\n");
+    inquirer.prompt(
+      [{
+        name: "itemId",
+        type: "input",
+        message: "Type the id of the item you want to buy",
+        validate: function (value) {
+          if (isNaN(value) === false) {
+            return true;
+          } else {
+            return false;
+          }
+        }
+      }, {
+        name: "quantity",
+        type: "input",
+        message: "How many do you want to buy?",
+        validate: function (value) {
+          if (isNaN(value) === false) {
+            return true;
+          } else {
+            return false;
+          }
+        }
+      }]
+    ).then(function (answer) {
+      checkInventory(answer.itemId, answer.quantity);
+    });
+  }
   //call the function to display products data in a table
-  displayInventory();
-  //Promt the user to select an item by its ID
-  console.log("Choose Item!");
-  inquirer.prompt(
-    [{
-      name: "itemId",
-      type: "input",
-      message: "Type the id of the item you want to buy",
-      validate: function (value)
-      {
-        if (isNaN(value) === false) {
-          return true;
-        } else {
-          return false;
-        }
-      }
-    }, {
-      name: "quantity",
-      type: "input",
-      message: "How many do you want to buy?",
-      validate: function (value)
-      {
-        if (isNaN(value) === false) {
-          return true;
-        } else {
-          return false;
-        }
-      }
-    }]
-  ).then(function (answer){
-    buyItem(answer.itemId, answer.quantity);
-  });
+  displayInventory(test);
 }
 
-function displayInventory(){
+function displayInventory(callback) {
   let data = [];
   let q = "SELECT item_id, product_name, price , stock_quantity FROM products";
-  connection.query(q, function (err, res){
+  connection.query(q, function (err, res) {
     if (err) throw err;
     data.push(["ID", "Product", "Price", "Quantity"]);
     res.forEach(element => {
@@ -63,15 +60,34 @@ function displayInventory(){
       data.push(row);
     });
     console.log(table(data));
+    callback();
   });
 }
 
-function buyItem(item, quantity){
+function buyItem(item, quantity) {
   // Update the amount of items in the inventory
   let q = "UPDATE products SET `stock_quantity`= `stock_quantity` - ? WHERE ?";
-  connection.query(q, [quantity, { item_id: item }], function (err, res){
+  connection.query(q, [quantity, { item_id: item }], function (err, res) {
     if (err) throw err;
     // console.log(res);
+    connection.query("SELECT price FROM products WHERE ?", { item_id: item }, function(err,res){
+      if(err) throw err;
+      let exp = res[0].price * quantity;
+      console.log(`Youe spent ${exp}`);
+    });
     start();
+  });
+}
+
+function checkInventory(item,amount) {
+  let q = "SELECT stock_quantity FROM products WHERE ?";
+  connection.query(q, {item_id: item}, function(err,res){
+    if(err) throw err;
+    if ((res[0].stock_quantity - amount) >= 0){
+      buyItem(item, amount);
+    } else{
+      console.log("We don't have enough items");
+      start();
+    }
   });
 }
